07f9a0b4aa6edb73391b318f67dd4ce84e5ba626
Ansh Goyal <anshgoyal0604@gmail.com.com>
2023-08-04 21:06:03 +0530

    project

--- App.js
+++ App.js
@@ -2,10 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log("hello ld")
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ height: 100, width: 100 , backgroundColor: 'red'}}>
+      <Text>Hello world</Text>
     </View>
   );
 }

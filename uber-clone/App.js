0e5aec54bc93228d584a9bcfb9b135c976902a21
Ansh goyal <anshgoyal0604@gmail.com.com>
2023-08-05 23:13:06 +0530

    project

--- uber-clone/App.js
+++ uber-clone/App.js
@@ -4,11 +4,35 @@ import { Provider } from "react-redux";
 import { store } from './store';
 import  HomeScreen  from "./screens/HomeScreen";
 
+import { NavigationContainer} from '@react-navigation/native' ;
+import { createStackNavigator } from '@react-navigation/stack';
+import MapScreen from "./screens/MapScreen";
+
+
+
 export default function App() {
+  const Stack = createStackNavigator();
+
   return (
-    <Provider store={store}>
-     <HomeScreen />
-    </Provider>
+   < Provider store={store} >
+     <NavigationContainer>
+      <Stack.Navigator>
+      <Stack.Screen 
+       name="HomeScreen"
+       component={HomeScreen}
+       options={{ headerShown: false,}}
+      />
+      <Stack.Screen 
+       name="MapScreen"
+       component={MapScreen}
+       options={{ headerShown: false,}}
+      />
+
+      </Stack.Navigator> 
+
+     {/*HomeScreen /> */}  
+     </NavigationContainer>       
+   </Provider>    
   );
 }
 
--- uber-clone/screens/HomeScreen.js
+++ uber-clone/screens/HomeScreen.js
@@ -1,12 +1,34 @@
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView , Image } from "react-native";
 import React from "react";
+import tw from "tailwind-react-native-classnames";
+import NavOptions from "../components/NavOptions";
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import {GOOGLE_MAPS_APIKEY}  from "@env";
+
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView>
-      <Text style={styles.text}> i Am THe homeScreen helo
+    <SafeAreaView style ={tw`bg-white h-full `}>
+      <View style={ tw`p-5`}>
+       <Image 
+        style={{
+          width:100, height:100, resizeMode: 'contain'
+        }}
+         source={{
+          uri: "https://links.papareact.com/gzs",
+         }}     
+       />
+
+       <GooglePlacesAutocomplete
+           placeholder="where from"
+           nearbyPlacesAPI="GooglePlacesSearch"
+           debounce={400}
+       />   
+
+
+       <NavOptions />
+      </View> 
       
-      </Text>
     </SafeAreaView>
   );
 };
@@ -15,6 +37,6 @@ export default HomeScreen;
 
 const styles = StyleSheet.create({
   text: {
-    color: 'blue'
+    color: 'red'
   }
 });
